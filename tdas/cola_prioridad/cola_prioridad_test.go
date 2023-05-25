b3b6e3167324575bde4fc5e943234a4f7792bdfa
edgartellom <edgar.tello@tecsup.edu.pe>
2023-05-25 19:17:17 -0300

    uso de variable global en pruebas

--- tdas/cola_prioridad/cola_prioridad_test.go
+++ tdas/cola_prioridad/cola_prioridad_test.go
@@ -12,6 +12,10 @@ import (
 
 var TAM_VOLUMEN = []int{12500, 25000, 50000, 100000, 200000, 400000}
 
+var ARREGLO_PRUEBAS_INT = []int{6, 4, 2, 6, 5, 1, 0, 9}
+
+var ARREGLO_PRUEBAS_STR = []string{"Gatito", "Perro", "Loro", "Sapo", "Raton", "Leon", "Vaca", "Burro"}
+
 func func_cmp_int(a, b int) int {
 	return a - b
 }
@@ -42,32 +46,31 @@ func TestUnElemento(t *testing.T) {
 
 func TestHeapEncolar(t *testing.T) {
 	t.Log("Encola algunos pocos elementos en el heap, y se comprueba que en todo momento funciona acorde")
-	arr := []int{6, 4, 2, 6, 5, 1, 0, 9}
 
 	heap := TDAHeap.CrearHeap(func_cmp_int)
 
-	heap.Encolar(arr[0])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[0])
 	require.EqualValues(t, 1, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[1])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[1])
 	require.EqualValues(t, 2, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[2])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[2])
 	require.EqualValues(t, 3, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[3])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[3])
 	require.EqualValues(t, 4, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[4])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[4])
 	require.EqualValues(t, 5, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[5])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[5])
 	require.EqualValues(t, 6, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[6])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[6])
 	require.EqualValues(t, 7, heap.Cantidad())
 	require.EqualValues(t, 6, heap.VerMax())
-	heap.Encolar(arr[7])
+	heap.Encolar(ARREGLO_PRUEBAS_INT[7])
 	require.EqualValues(t, 8, heap.Cantidad())
 	require.EqualValues(t, 9, heap.VerMax())
 }
@@ -75,10 +78,9 @@ func TestHeapEncolar(t *testing.T) {
 func TestHeapDesencolar(t *testing.T) {
 	t.Log("Encola algunos pocos elementos en el heap, y los desencola, revisando que en todo momento " +
 		"el heap se comporte de manera adecuada")
-	arr := []int{6, 4, 2, 6, 5, 1, 0, 9}
 	heap := TDAHeap.CrearHeap(func_cmp_int)
 
-	for i, elem := range arr {
+	for i, elem := range ARREGLO_PRUEBAS_INT {
 		heap.Encolar(elem)
 		require.EqualValues(t, i+1, heap.Cantidad())
 	}
@@ -96,9 +98,8 @@ func TestHeapDesencolar(t *testing.T) {
 func TestConStrings(t *testing.T) {
 	t.Log("Valida que no solo funcione con enteros")
 	heap := TDAHeap.CrearHeap(func_cmp_str)
-	arr := []string{"Gatito", "Perro", "Loro", "Sapo", "Raton", "Leon", "Vaca", "Burro"}
 
-	for i, elem := range arr {
+	for i, elem := range ARREGLO_PRUEBAS_STR {
 		heap.Encolar(elem)
 		require.EqualValues(t, i+1, heap.Cantidad())
 	}
@@ -115,12 +116,11 @@ func TestConStrings(t *testing.T) {
 
 func TestHeapify(t *testing.T) {
 	t.Log("Valida que se cree un heap apartir de un arreglo y que funcione correctamente")
-	arr := []int{6, 4, 2, 6, 5, 1, 0, 9}
 
-	heap := TDAHeap.CrearHeapArr(arr, func_cmp_int)
+	heap := TDAHeap.CrearHeapArr(ARREGLO_PRUEBAS_INT, func_cmp_int)
 
 	require.EqualValues(t, 9, heap.VerMax())
-	for i := 0; i < len(arr); i++ {
+	for i := 0; i < len(ARREGLO_PRUEBAS_INT); i++ {
 		require.EqualValues(t, heap.VerMax(), heap.Desencolar())
 	}
 	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.VerMax() })
@@ -129,10 +129,9 @@ func TestHeapify(t *testing.T) {
 
 func TestHeapSort(t *testing.T) {
 	t.Log("Valida que HeapSort ordene el arreglo correctamente y sea in-place")
-	arr := []int{6, 4, 2, 6, 5, 1, 0, 9}
 
-	TDAHeap.HeapSort(arr, func_cmp_int)
-	require.EqualValues(t, []int{0, 1, 2, 4, 5, 6, 6, 9}, arr)
+	TDAHeap.HeapSort(ARREGLO_PRUEBAS_INT, func_cmp_int)
+	require.EqualValues(t, []int{0, 1, 2, 4, 5, 6, 6, 9}, ARREGLO_PRUEBAS_INT)
 }
 
 func ejecutarPruebaVolumen(b *testing.B, n int) {
